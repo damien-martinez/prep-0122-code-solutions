43ab9b0bc8e7fbf45bf2a5e3fcebbcf326357c68
Damien Martinez <mart.damien1@gmail.com>
2022-01-18 18:12:35 -0800

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array is as follows: ', languages);
+
+var firstElement = languages[0];
+console.log('The first element of the languages array is', firstElement);
+
+var thirdElement = languages[2];
+console.log('The third element of the languages array is', thirdElement);
+
+var length = languages.length;
+console.log('The length of the array is ', length);
+
+var lastElement = languages[languages.length - 1];
+console.log('The last element of the languages array is', lastElement);
