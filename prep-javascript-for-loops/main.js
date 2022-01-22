1f5f75f9f692d84fde5edd960a9faa8d1f06d06b
Damien Martinez <mart.damien1@gmail.com>
2022-01-21 16:20:51 -0800

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var i = 0, j = 0; i < 10; i += 1) {
+  console.log(j);
+  j += 2;
+}
+
+for (var i = 100; i > 0; i--) {
+  console.log('Time till explosion: ', i);
+}
